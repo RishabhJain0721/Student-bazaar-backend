9933ba6ff95adf975ab9da3c04e9f3943cd446b8
Rishabh <rishujain0721@gmail.com>
2023-11-29 20:34:04 +0530

    Added verification link

--- controllers/AuthController.js
+++ controllers/AuthController.js
@@ -73,8 +73,7 @@ const signup = async (req, res) => {
     console.log("Saved new user : ", newUser)
 
     // Send a verification email to the user as the admin
-    // const verificationLink = `https://dorm-deals-frontend.vercel.app/verify-email?token=${verificationToken}`;
-    const verificationLink= `http://localhost:5173/verification?token=${verificationToken}`
+    const verificationLink= `https://student-bazaar.vercel.app/verification?token=${verificationToken}`
 
     await transporter.sendMail({
       from: "rishujain0721@gmail.com",
