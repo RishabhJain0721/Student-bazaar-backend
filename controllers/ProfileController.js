478d1172044804302991e054338168bc0c0dd03a
Rishabh <rishujain0721@gmail.com>
2023-12-16 19:13:28 +0530

    Modified profile data sending

--- controllers/ProfileController.js
+++ controllers/ProfileController.js
@@ -6,6 +6,28 @@ const ProfileDetails = async (req, res) => {
     const user = await User.findOne({
       verificationToken: req.body.token,
     }).exec();
+
+    if (
+      req.body.token ===
+      "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJlbWFpbCI6IjIxMDA1MjE1MjAwNDFAaWV0bHVja25vdy5hYy5pbiIsImlhdCI6MTcwMTEwODc5MiwiZXhwIjoxNzAxMTk1MTkyfQ.tuoLoyp6HZLgUTqtQy1QTTA5P4Qlc_1uKGO0RRwYtzM"
+    ) {
+      try {
+        const items = await ItemToSell.aggregate([
+          {
+            $project: {
+              itemName: 1,
+              itemCost: 1,
+              userName: 1,
+              image: { $arrayElemAt: ["$images", 0] },
+            },
+          },
+        ]);
+        return res.status(200).send({ user, items });
+      } catch (error) {
+        console.error("Error:", error);
+        return res.status(500).send("Internal server error");
+      }
+    }
     return res.status(200).send(user);
   } catch (error) {
     console.error("Error:", error);
@@ -21,16 +43,15 @@ const ItemsListedByAdmin = async (req, res) => {
           itemName: 1,
           itemCost: 1,
           userName: 1,
-          image: { $arrayElemAt: ["$images", 0] }
-        }
-      }
+          image: { $arrayElemAt: ["$images", 0] },
+        },
+      },
     ]);
     return res.status(200).send(items);
   } catch (error) {
     console.error("Error:", error);
     return res.status(500).send("Internal server error");
   }
-  
 };
 
 export { ProfileDetails, ItemsListedByAdmin };
