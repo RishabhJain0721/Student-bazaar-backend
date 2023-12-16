c8e83281fd4819b921afba00144a26fcb9be5321
Rishabh Jain <107767172+RishabhJain0721@users.noreply.github.com>
2023-12-16 11:05:19 +0530

    Update index.js

--- index.js
+++ index.js
@@ -9,13 +9,13 @@ const app = express();
 const PORT = process.env.PORT || 5000;
 const PASSWORD = process.env.PASS;
 
-// app.use(cors());
-app.use((req, res, next) => {
-  res.header('Access-Control-Allow-Origin', 'https://studentbazaar.in');
-  res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
-  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
-  next();
-});
+app.use(cors());
+// app.use((req, res, next) => {
+//   res.header('Access-Control-Allow-Origin', 'https://studentbazaar.in');
+//   res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+//   res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+//   next();
+// });
 
 app.use(express.json());
 
