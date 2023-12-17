0bee778ff555e1cbcaf7447a044f8a03ff8571a9
Rishabh <rishujain0721@gmail.com>
2023-12-17 16:49:56 +0530

    testing hosted server

--- controllers/AdminListingsController.js
+++ controllers/AdminListingsController.js
@@ -1,22 +1,31 @@
+// import ItemToSell from "../models/SellModel.js";
+
+// const ItemsListedByAdmin = async (req, res) => {
+//     try {
+//       const items = await ItemToSell.aggregate([
+//         {
+//           $project: {
+//             itemName: 1,
+//             itemCost: 1,
+//             userName: 1,
+//             image: { $arrayElemAt: ["$images", 0] },
+//           },
+//         },
+//       ]);
+//       return res.status(200).send(items);
+//     } catch (error) {
+//       console.error("Error:", error);
+//       return res.status(500).send("Internal server error");
+//     }
+//   };
+
+//   export { ItemsListedByAdmin };
+
 import ItemToSell from "../models/SellModel.js";
 
-const ItemsListedByAdmin = async (req, res) => {
-    try {
-      const items = await ItemToSell.aggregate([
-        {
-          $project: {
-            itemName: 1,
-            itemCost: 1,
-            userName: 1,
-            image: { $arrayElemAt: ["$images", 0] },
-          },
-        },
-      ]);
-      return res.status(200).send(items);
-    } catch (error) {
-      console.error("Error:", error);
-      return res.status(500).send("Internal server error");
-    }
-  };
+const ItemsListedByAdmin =async (req,res) =>{
+  console.log("Inside ItemsListedByAdmin");
+  res.status(200).send("Inside ItemsListedByAdmin");
+}
 
-  export { ItemsListedByAdmin };
+export {ItemsListedByAdmin};
